637b0a78c2443fdf5e4b87b56efeedfa8e57855c
krishnaanand2908 <krishnaanand290809@gmail.com>
2022-04-08 13:25:00 +0530

    LOL

---
+++ certificate.py
@@ -0,0 +1,38 @@
+name = input("Enter your name here:")
+
+sex = input("Enter your sex [M/F] here:")
+
+bgroup = input("Enter your blood group here:")
+
+sport = input("Enter your favourite sport here:")
+
+rank = int(input("Enter the rank you got in your favourite sport here:"))
+
+print("--------------------------------------")
+
+print("|            CERTIFICATE             | ")
+
+print("| Name:", name, "                    |")  
+
+if sex == "M":
+    print("| Sex: Male                                              ")
+else:
+    print("| Sex: Female                                   ")
+    
+print("| Blood group", bgroup, "                         ") 
+print("| Sport:", sport, "                               ") 
+print("| Rank achived:", rank, "                        ") 
+print("--------------------------------------") 
+    
+
+
+
+    
+
+    
+
+    
+
+    
+
+
